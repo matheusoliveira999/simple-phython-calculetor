bcc1a66cb1bfd8a1ce578202dfb18e9bdac13053
matheusoliveira999 <167232576+matheusoliveira999@users.noreply.github.com>
2024-08-27 14:14:19 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+def  soma(a, b):
+      return a + b 
+if name=="main":
+  num1 = float(inpunt("digite o primeiro número "))
+  num2 = float(inpunt("digite o segundo número: "))
+  resultado = soma(num1, num2)
+  print(f"resultado: {resultado}")
